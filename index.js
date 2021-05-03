2c57f7c035a4294eaa232a8c30a063629dcb9092
Kunal Dubey <kunaldubey2297@gmail.com>
2021-05-03 11:59:05 +0530

    added timestamp as a part of log data

--- index.js
+++ index.js
@@ -5,9 +5,10 @@ const hot = {
   log(message, options){
     const api_key = process.env.HOT_LOG_KEY;
     if (!api_key) throw "HOT_LOG_KEY is not defined in the ENV";
-    console.log("API KEY is", api_key);
+    console.log("API KEY is", api_key); 
+    const timeStamp = new Date().toISOString();
     try {
-        axios.post('http://localhost:2297/log',{api_key,message,options}).then(()=>null,err=> {throw err})
+        axios.post('http://localhost:2297/log',{api_key,timeStamp,message,options}).then(()=>null,err=> {throw err})
         .catch(err=> {throw err})
     } catch (error) {
       console.log("Logging Failed due to", error);
