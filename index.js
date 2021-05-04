d5edd2e37cbd426b1f6910bd02df50119c5b8121
Kunal Dubey <kunaldubey2297@gmail.com>
2021-05-04 10:14:34 +0530

    added url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const hot = {
     if (!api_key) throw "HOT_LOG_KEY is not defined in the ENV";
     // console.log("API KEY is", api_key); 
     const timeStamp = new Date().toISOString();
-        axios.post('http://localhost:2297/log',{api_key,timeStamp,message,options}).then(()=>null,err=> {throw err})
+        axios.post('https://hotlog-server.azurewebsites.net/log',{api_key,timeStamp,message,options}).then(()=>null,err=> {throw err})
         .catch(err=> {throw err})
     } catch (error) {
       console.log("Logging Failed due to", error);
